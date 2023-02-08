57b3a58dd3ff26b5ed5520a01a5f01382e526541
Carlos Cid <hsm.carlos89@gmail.com>
2023-02-08 15:02:01 -0300

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BaseColaboradores } from "./components/BaseColaboradores";
 import { useState } from "react";
 function App() {
-  
+
   const [listaColaboradores, setListaColaboradores] = useState(BaseColaboradores)
   const [nombre, setNombre] = useState("")
   const [correo, setCorreo] = useState("")
-  const [disable,setDisable] = useState(true)
-  const [filtro,setFiltro] = useState("")
-  
+  const [disable, setDisable] = useState(true)
+  const [listaFiltrada, setlistaFiltrada] = useState(listaColaboradores)
+
+
 
   const obtenerId = () => {
     const last = listaColaboradores[listaColaboradores.length - 1];
@@ -15,7 +16,7 @@ function App() {
     const id = numero.toString()
     return id
   }
-  
+
 
   const capturaNombre = (e) => {
     disableButton()
@@ -27,29 +28,31 @@ function App() {
   }
   const enviarFormulario = (e) => {
     e.preventDefault()
-    setListaColaboradores([...listaColaboradores, { id: obtenerId() ,nombre: nombre, correo: correo }])
+    setListaColaboradores([...listaColaboradores, { id: obtenerId(), nombre: nombre, correo: correo }])
+    setlistaFiltrada([...listaColaboradores, { id: obtenerId(), nombre: nombre, correo: correo }])
     setNombre("")
     setCorreo("")
     setDisable(true)
-}
-const disableButton = () => {
-  if (correo === '' || nombre === '') {
+  }
+  const disableButton = () => {
+    if (correo === '' || nombre === '') {
       setDisable(true)
       return
-  }
-  else {
+    }
+    else {
       setDisable(false)
+    }
   }
-}
-const filtraColaborador = (nombre) => {
-  const listaFiltrada = listaColaboradores.filter(el => el.nombre === nombre)
-  setListaColaboradores(listaFiltrada)
-  setFiltro("")
-}
+  const filtrarColaborador = (e) => {
 
-  const capturaFilter = (e) => {
-    setFiltro(e.target.value)
-  }
+    const query = e.target.value;
+    var nuevaLista = [...listaFiltrada];
+    nuevaLista = nuevaLista.filter((item) => {
+      return item.nombre.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    setListaColaboradores(nuevaLista);
+  };
+
 
 
 
@@ -61,8 +64,8 @@ const filtraColaborador = (nombre) => {
       <h1 className="text-light">Desafio Colaboradores</h1>
       <div className="container fs-4 text">
         <div className="buscar">
-           <p><button onClick={() => filtraColaborador(filtro) } className="btn btn-dark" >Filtrar</button></p>
-          <input value={filtro} onChange={capturaFilter} type="text" className="form-control m-4 w-50" placeholder="Nombre" />
+          <p>Filtrar</p>
+          <input onChange={filtrarColaborador} type="text" className="form-control m-4 w-50" placeholder="Nombre" />
         </div>
         <form onSubmit={enviarFormulario}>
           <p>Nombre del colaborador</p>
@@ -73,9 +76,10 @@ const filtraColaborador = (nombre) => {
         </form>
         <h1 className="border-bottom">Listado de colaboradores</h1>
         <ul className="my-3">
-          {listaColaboradores.map(colaborador =>
-            <li key={colaborador.id}>{colaborador.nombre}-{colaborador.correo}</li> )}
-        </ul>        
+          {listaColaboradores.map((item, index) => (
+            <li key={item.id}>{item.nombre}-{item.correo}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
